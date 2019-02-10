a2d6b1a9a02be68c965e7ee910a923bc8d1248be
ish-vlad <vladislav.ishimtsev@skoltech.ru>
2019-02-10 05:54:07 +0300

    Interactive Bar Chart (like Trend Lines)

--- static/js/visualization.js
+++ static/js/visualization.js
@@ -1,30 +1,57 @@
-function chart_bar(data) {
+function chart_bar(data, data_full) {
+    var config = {
+        type: 'bar',
+        data: data,
+        options: {
+            title: {
+                display: true,
+                text: 'Chart.js Bar Chart - Stacked'
+            },
+            tooltips: {
+                mode: 'index',
+                intersect: false
+            },
+            responsive: true,
+            scales: {
+                xAxes: [{
+                    stacked: true,
+                }],
+                yAxes: [{
+                    stacked: true
+                }]
+            }
+        }
+    }
+
     window.addEventListener("load",function(event) {
         var ctx = document.getElementById('myChart').getContext('2d');
-        window.myBar = new Chart(ctx, {
-            type: 'bar',
-            data: data,
-            options: {
-                title: {
-                    display: true,
-                    text: 'Chart.js Bar Chart - Stacked'
-                },
-                tooltips: {
-                    mode: 'index',
-                    intersect: false
-                },
-                responsive: true,
-                scales: {
-                    xAxes: [{
-                        stacked: true,
-                    }],
-                    yAxes: [{
-                        stacked: true
-                    }]
-                }
-            }
-        });
+        window.myBar = new Chart(ctx, config);
     }, false);
+
+    document.getElementById('addDataBar').addEventListener('click', function() {
+        if (config.data.labels.length < data_full.labels.length) {
+            idx = data_full.labels.length - config.data.labels.length - 1
+            config.data.labels.unshift(data_full.labels[idx]);
+
+            for (i = 0; i < data.datasets.length; i++) {
+                config.data.datasets[i].data.unshift(data_full.datasets[i][idx]);
+            }
+
+            window.myBar.update();
+        }
+    });
+
+    document.getElementById('removeDataBar').addEventListener('click', function() {
+        if (config.data.labels.length > 0) {
+            config.data.labels.shift(); // remove the label first
+
+            config.data.datasets.forEach(function(dataset) {
+                dataset.data.shift();
+            });
+
+            window.myBar.update();
+        }
+    });
 }
 
 function trend_line(data, data_full) {

62dcd74b547fa5efb64b04c017541b36e861e0c1
dvolkhonskiy <dvolkhonskiy@gmail.com>
2019-02-16 09:08:17 +0300

    views global refactoring

--- articles/urls.py
+++ articles/urls.py
@@ -3,16 +3,20 @@ from django.urls import path
 from articles import views
 
 urlpatterns = [
-    path('', views.articles, name='articles'),
-    path('details/<article_id>', views.article_details, name='article_details'),
-    path('authors/<author_name>', views.author_articles, name='author_articles'),
+    path('', views.ArticlesView.as_view(), name='articles'),
+    path('details/<int:pk>', views.ArticleDetailsView.as_view(), name='article_details'),
+    path('authors/<author_name>', views.ArticlesOfAuthor.as_view(), name='author_articles'),
+    path('library', views.ArticlesLibrary.as_view(), name='library'),
+
+    path('liked', views.LikedDisliked.as_view(), name='liked'),
+    path('disliked', views.LikedDisliked.as_view(), name='disliked'),
+
     path('api/v1/library/add/<article_id>', views.add_remove_from_library, name='lib_add'),
     path('api/v1/library/remove/<article_id>', views.add_remove_from_library, name='lib_remove'),
     path('api/v1/note/update/<article_id>', views.change_note, name='change_note'),
     path('api/v1/like/<article_id>', views.like_dislike, name='like'),
     path('api/v1/dislike/<article_id>', views.like_dislike, name='dislike'),
     # path('api/articles/search/<searchinput>', articles_views.search, name='search'),
-    path('library', views.library, name='library'),
-    path('liked', views.liked_disliked, name='liked'),
-    path('disliked', views.liked_disliked, name='disliked'),
+
+
 ]

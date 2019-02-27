e09f4b609edda197ce41e24548f690fdc3488a97
dvolkhonskiy <dvolkhonskiy@gmail.com>
2019-02-27 19:32:03 +0300

    registration/login works

---
+++ core/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+
+from articles.views import home, landing_view
+# from core.forms import UserLoginForm
+
+
+urlpatterns = [
+    path('', landing_view, name='landing'),
+    path('home/', home, name='home'),
+    path('accounts/', include('allauth.urls')),
+    path('auth/', include('social_django.urls', namespace='social'))
+]
--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 rm -rf db.sqlite3
-rm -rf articles/migrations
+rm -rf articles/migrations core/migrations
 python manage.py makemigrations
 python manage.py makemigrations articles
+python manage.py makemigrations core
 python manage.py migrate
-python scripts/increment.py
+#python scripts/increment.py --max_articles 50
 
-python manage.py createsuperuser
+# python manage.py createsuperuser

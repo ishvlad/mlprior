4d783a2ee326015f03a0b473b12378b8f1ab95bd
dvolkhonskiy <dvolkhonskiy@gmail.com>
2019-03-10 13:49:50 +0300

    search by abstract and titile

--- search/views.py
+++ search/views.py
@@ -5,7 +5,8 @@ from search.documents import ArticleDocument
 class ArticlesSearchView(ArticlesView):
     def get_queryset(self):
         q = self.request.GET.get('q')
-        articles = ArticleDocument.search().query('match', title=q)
+        articles = ArticleDocument.search().query('multi_match', query=q, fields=['title', 'abstract'])
+        articles = articles.to_queryset()
         return articles
 
     def get_context_data(self, **kwargs):

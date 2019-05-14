ab696a8a75b738c7cb6e4d34888bb39b70e8adc8
dvolkhonskiy <dvolkhonskiy@gmail.com>
2019-05-14 10:24:47 +0300

    static files

--- mlprior/urls.py
+++ mlprior/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 import search.views as search_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +25,4 @@ urlpatterns = [
     path('articles/', include('articles.urls')),
     path('', include('search.urls')),
     path('', include('log.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

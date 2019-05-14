66187643092f45829227cd71c2faa39e8fb3c13d
dvolkhonskiy <dvolkhonskiy@gmail.com>
2019-05-14 10:40:23 +0300

    static files

--- mlprior/settings/base.py
+++ mlprior/settings/base.py
@@ -149,7 +149,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticroot/'
 
 LOGIN_URL = 'accounts/login'
 

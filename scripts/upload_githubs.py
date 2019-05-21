f2c537f1bfae342ca0dbc4dacac2a40d7efa7363
dvolkhonskiy <dvolkhonskiy@gmail.com>
2019-05-21 16:32:32 +0300

    github add repos

--- scripts/upload_githubs.py
+++ scripts/upload_githubs.py
@@ -6,6 +6,7 @@ sys.path.append('./')
 
 from services.github.helpers import send_github_url_to_server
 import json
+from time import sleep
 
 
 with open('./scripts/links-between-papers-and-code.json', 'r') as f:
@@ -13,10 +14,12 @@ with open('./scripts/links-between-papers-and-code.json', 'r') as f:
 
 futures = []
 
-for i in range(5):
+for i in range(len(data)):
     print('Add GitHub for', data[i]['paper_arxiv_id'])
-    asyncio.sleep(1)
+    if data[i]['paper_arxiv_id'] is None:
+        print('Skipping None')
+        continue
     futures.append(send_github_url_to_server(data[i]['repo_url'], data[i]['paper_arxiv_id']))
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(asyncio.wait(futures))
+# loop = asyncio.get_event_loop()
+# loop.run_until_complete(asyncio.wait(futures))

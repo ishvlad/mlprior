b0940f3bbd9851fb8a8a1281ded839109d815054
dvolkhonskiy <dvolkhonskiy@gmail.com>
2019-07-12 09:11:19 +0300

    add COARSE from 4000 port (for angular universe)

--- mlprior/settings/base.py
+++ mlprior/settings/base.py
@@ -74,6 +74,8 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.facebook',
 
+    'django_celery_beat',
+
 ]
 
 MIDDLEWARE = [
@@ -290,6 +292,8 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:8000',
     'http://localhost:4200',
     'http://127.0.0.1:4200',
+    'http://localhost:4000',
+    'http://127.0.0.1:4000',
     'http://mlprior.com'
 ]
 

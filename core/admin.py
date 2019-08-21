ff6b5a15ec727348a51acc6e4baddc12542c12ba
dvolkhonskiy <dvolkhonskiy@gmail.com>
2019-08-21 18:33:46 +0300

    subscription to admin

--- core/admin.py
+++ core/admin.py
@@ -2,8 +2,9 @@
 # users/admin.py
 from django.contrib import admin
 
-from .models import Feedback, User, Profile
+from .models import Feedback, User, Profile, PremiumSubscription
 
 admin.site.register(Feedback)
 admin.site.register(User)
 admin.site.register(Profile)
+admin.site.register(PremiumSubscription)

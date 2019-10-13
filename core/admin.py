c60ae2c0784db1bf89d7e260d6ce4c3000b025b6
dvolkhonskiy <dvolkhonskiy@gmail.com>
2019-10-13 19:16:58 +0300

    add file upload to admin

--- core/admin.py
+++ core/admin.py
@@ -2,10 +2,11 @@
 # users/admin.py
 from django.contrib import admin
 
-from .models import Feedback, User, Profile, PremiumSubscription, RequestDemo
+from .models import Feedback, User, Profile, PremiumSubscription, RequestDemo, FileUpload
 
 admin.site.register(Feedback)
 admin.site.register(User)
 admin.site.register(Profile)
 admin.site.register(PremiumSubscription)
 admin.site.register(RequestDemo)
+admin.site.register(FileUpload)
